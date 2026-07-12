b26bac3b8041f13a56d60f9b68a484b9b1a4e83b
Moussa DIABY <diaby0359@gmail.com>
2026-07-12 10:52:44 +0200

    Ajout de la configuration YAML pour les audits

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
 from database import get_engine
 from quality_check import run_generic_rules
+from config import load_config
 
 
 def main():
 
     print("Démarrage GeoAudit")
+
+    # Chargement de la configuration
+    config = load_config()
+
+    # Récupération de la table à auditer
+    table_name = config["audit"]["table_name"]
+
+    print(f"Couche analysée : {table_name}")
+
 
     # Test connexion PostgreSQL/PostGIS
     try:
@@ -18,10 +28,6 @@ def main():
         print("Erreur de connexion PostgreSQL :")
         print(e)
         return
-
-
-    # Table à auditer
-    table_name = "iris"
 
 
     # Lancement des règles génériques
